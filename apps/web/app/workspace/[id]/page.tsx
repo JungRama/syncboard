db8beadba037088f7bcd33f8324ca7ad5875ca0a
Jung Rama <jungrama.id@gmail.com>
2023-11-11 21:55:20 +0800

    chore(fix): getsnapshot and loadsnapshot for whiteboard

--- apps/web/app/workspace/[id]/page.tsx
+++ apps/web/app/workspace/[id]/page.tsx
@@ -13,7 +13,120 @@ export default function Page(): JSX.Element {
 
 	const [storeEvents, setStoreEvents] = useState<string[]>([])
 	
-	const defaultValue = [{"gridSize":10,"name":"","meta":{},"id":"document:document","typeName":"document"},{"id":"pointer:pointer","typeName":"pointer","x":256,"y":384.0000305175781,"lastActivityTimestamp":1699709190832,"meta":{}},{"meta":{},"id":"page:page","name":"Page 1","index":"a1","typeName":"page"},{"x":0,"y":0,"z":1,"meta":{},"id":"camera:page:page","typeName":"camera"},{"editingShapeId":null,"croppingShapeId":null,"selectedShapeIds":["shape:vz5kg_kh77BiAZRyhWFGP"],"hoveredShapeId":null,"erasingShapeIds":[],"hintingShapeIds":[],"focusedGroupId":null,"meta":{},"id":"instance_page_state:page:page","pageId":"page:page","typeName":"instance_page_state"},{"followingUserId":null,"opacityForNextShape":1,"stylesForNextShape":{},"brush":null,"scribble":null,"cursor":{"type":"cross","rotation":0},"isFocusMode":false,"exportBackground":true,"isDebugMode":true,"isToolLocked":false,"screenBounds":{"x":0,"y":0,"w":1035.2000732421875,"h":653.6000366210938},"zoomBrush":null,"isGridMode":false,"isPenMode":false,"chatMessage":"","isChatting":false,"highlightedUserIds":[],"canMoveCamera":true,"isFocused":true,"devicePixelRatio":1.25,"isCoarsePointer":false,"isHoveringCanvas":true,"openMenus":[],"isChangingStyle":false,"isReadonly":false,"meta":{},"id":"instance:instance","currentPageId":"page:page","typeName":"instance"},{"x":256,"y":384.0000305175781,"rotation":0,"isLocked":false,"opacity":1,"meta":{},"id":"shape:vz5kg_kh77BiAZRyhWFGP","type":"arrow","parentId":"page:page","index":"a1","props":{"dash":"draw","size":"m","fill":"none","color":"black","labelColor":"black","bend":0,"start":{"type":"point","x":0,"y":0},"end":{"type":"point","x":2,"y":0},"arrowheadStart":"none","arrowheadEnd":"arrow","text":"","font":"draw"},"typeName":"shape"}]
+	const defaultValue = {
+   "store":{
+      "document:document":{
+         "gridSize":10,
+         "name":"",
+         "meta":{
+            
+         },
+         "id":"document:document",
+         "typeName":"document"
+      },
+      "page:page":{
+         "meta":{
+            
+         },
+         "id":"page:page",
+         "name":"Page 1",
+         "index":"a1",
+         "typeName":"page"
+      },
+      "shape:YpkCnpjDq2b5hmKOeafIR":{
+         "x":357.8947448730469,
+         "y":374.73687744140625,
+         "rotation":0,
+         "isLocked":false,
+         "opacity":1,
+         "meta":{
+            
+         },
+         "id":"shape:YpkCnpjDq2b5hmKOeafIR",
+         "type":"arrow",
+         "parentId":"page:page",
+         "index":"a1",
+         "props":{
+            "dash":"draw",
+            "size":"m",
+            "fill":"none",
+            "color":"black",
+            "labelColor":"black",
+            "bend":0,
+            "start":{
+               "type":"point",
+               "x":0,
+               "y":0
+            },
+            "end":{
+               "type":"point",
+               "x":2,
+               "y":0
+            },
+            "arrowheadStart":"none",
+            "arrowheadEnd":"arrow",
+            "text":"",
+            "font":"draw"
+         },
+         "typeName":"shape"
+      }
+   },
+   "schema":{
+      "schemaVersion":1,
+      "storeVersion":4,
+      "recordVersions":{
+         "asset":{
+            "version":1,
+            "subTypeKey":"type",
+            "subTypeVersions":{
+               "image":2,
+               "video":2,
+               "bookmark":0
+            }
+         },
+         "camera":{
+            "version":1
+         },
+         "document":{
+            "version":2
+         },
+         "instance":{
+            "version":21
+         },
+         "instance_page_state":{
+            "version":5
+         },
+         "page":{
+            "version":1
+         },
+         "shape":{
+            "version":3,
+            "subTypeKey":"type",
+            "subTypeVersions":{
+               "group":0,
+               "text":1,
+               "bookmark":1,
+               "draw":1,
+               "geo":7,
+               "note":4,
+               "line":1,
+               "frame":0,
+               "arrow":1,
+               "highlight":0,
+               "embed":4,
+               "image":2,
+               "video":1
+            }
+         },
+         "instance_presence":{
+            "version":5
+         },
+         "pointer":{
+            "version":1
+         }
+      }
+   }
+}
 	const defaultVal = {
 		store: {
 			...defaultValue
@@ -33,7 +146,6 @@ export default function Page(): JSX.Element {
 			if (change.source === 'user') {
 				// Added
 				for (const record of Object.values(change.changes.added)) {
-					console.log(JSON.stringify(editor?.store.allRecords()));
 					if (record.typeName === 'shape') {
 						logChangeEvent(`created shape (${record.type})`)
 
@@ -59,13 +171,22 @@ export default function Page(): JSX.Element {
 				}
 			}
     })
-  }, [editor])
+	}, [editor])
+	
+	const getSnap = () => {
+		console.log(JSON.stringify(editor?.store.getSnapshot()));
+	}
 
   return (
-    <div className='h-[100vh]'>
+		<div className='h-[100vh]'>
+			<div>
+				asdad
+				<button onClick={getSnap}>get snap</button>
+			</div>
     
-    {/* {JSON.stringify(editor)} */}
-    <Tldraw snapshot={jsonSnapshot} onMount={setAppToState} />
+			{/* {JSON.stringify(editor)} */}
+			{/* snapshot={jsonSnapshot} */}
+    <Tldraw snapshot={jsonSnapshot}  onMount={setAppToState} />
     </div>
   )
 }
